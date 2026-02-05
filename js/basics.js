9fe8b75f0404bebaa3cc20badb10b063e30a845c
Anmol Srivastava <srivastavaanmol977@gmail.com>
2026-02-05 16:23:44 +0530

    advance basics

--- js/basics.js
+++ js/basics.js
@@ -1,6 +1,26 @@
-let nul=null;
-let bool=true;
-let und;
-console.log(typeof(nul));
-let str="Hello wtaer";
-console.log(typeof(str));
+let today = new Date();
+console.log(today);
+console.time("test");
+for(let i=0;i<10000;i++){}
+console.timeEnd("test");
+
+console.group("Group1");
+console.log("Anmol")
+console.log("Anand")
+console.log("Gopal")
+console.groupEnd("Group1");
+
+
+console.count("a");
+console.count("a");
+console.count("b");
+console.count("a");
+console.count("b");
+console.countReset("a");
+console.count("a");
+
+//table
+let student = {name:"Anmol",age:20,course:"btech"};
+console.table(student);
+
+
